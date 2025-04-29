80c90a5d370f9890441ab17729fb61aac9489cc6
bth123 <m1207pro@gmail.com>
2025-04-29 21:06:29 +0300

    Prefix fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cogs = [SayCommand, LinkCommand, RapeCommand, DebugCommand, FAQs]
 
 class SLBot(commands.Bot):
     def __init__(self, *, intents: discord.Intents, command_prefix:str):
-        super().__init__(intents=intents, command_prefix=commands.when_mentioned_or(command_prefix), case_insensitive = True)
+        super().__init__(intents=intents, command_prefix=command_prefix, case_insensitive = True)
     
     async def setup_hook(self):
         for cog in cogs:
@@ -19,6 +19,6 @@ class SLBot(commands.Bot):
         print(f"User: {bot.user} (ID: {bot.user.id})")
 
 intents = discord.Intents.all()
-bot = SLBot(command_prefix=["$sudo ", "!"], intents=intents)
+bot = SLBot(command_prefix=commands.when_mentioned_or("$sudo ", "!"), intents=intents)
 
 bot.run(config.DISCORD_API_TOKEN)
