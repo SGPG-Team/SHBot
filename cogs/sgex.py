44a0a84a498661f8739750290fa112a43449eb5a
bth123 <m1207pro@gmail.com>
2025-06-01 21:57:48 +0300

    ⚡⚡ FIXED S.GEX

--- cogs/sgex.py
+++ cogs/sgex.py
@@ -7,5 +7,6 @@ class SGexCommand(commands.Cog):
 	@commands.Cog.listener("on_message")
 	async def sgex(self, msg):
 		if msg.content == "s.gex":
-		    await msg.channel.send("https://cdn.discordapp.com/attachments/1346554594238533652/1376256682220322846/saygex.gif")
+			file = discord.File("assets/saygex.gif")
+			await msg.channel.send(file=file)
 
